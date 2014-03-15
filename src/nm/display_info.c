f86523baa511bc9e12acdd4b4a54f769f0e0a715
izissise <hugues.m@hotmail.fr>
2014-03-15 23:28:27 +0100

    stock all symbols addr in a tab

--- src/nm/display_info.c
+++ src/nm/display_info.c
@@ -10,8 +10,9 @@
 
 #include "nm.h"
 
-void	dump_section_symbol(t_elf *elf, int sh, char *symstr, t_file *file)
+void	**list_symbols(t_elf *elf, int sh, t_file *file)
 {
+  void	**res;
   void	*sect;
   size_t	size;
   int	symsize;
@@ -20,14 +21,34 @@ void	dump_section_symbol(t_elf *elf, int sh, char *symstr, t_file *file)
   sect = file->data + elf->sh_offset(elf->elf, sh, file);
   size = elf->sh_size(elf->elf, sh, file);
   if ((sect == file->data) || (size == 0))
-    return ;
+    return (NULL);
   i = 0;
   symsize = (elf->type == ELFCLASS32) ? sizeof(Elf32_Sym) : sizeof(Elf64_Sym);
+  if ((res = malloc(((size / symsize) + 1) * sizeof(void*))) == NULL)
+    return (NULL);
   while ((size_t)(symsize * i) < size)
     {
-      elf->dump_symbol(sect + (symsize * i), symstr, file);
+      res[i] = sect + (symsize * i);
       i++;
     }
+  res[i] = NULL;
+  return (res);
+}
+
+void	dump_section_symbol(t_elf *elf, int sh, char *symstr, t_file *file)
+{
+  void	**symbols;
+  int	i;
+
+  i = 0;
+  if ((symbols = list_symbols(elf, sh, file)) == NULL)
+    return ;
+  while (symbols[i])
+    {
+      elf->dump_symbol(symbols[i], symstr, file);
+      i++;
+    }
+  free(symbols);
 }
 
 int	display_file(const char *filename)
--- src/section64.c
+++ src/section64.c
@@ -48,7 +48,7 @@ void		dump_symbol64(void *addr, char *symstr, t_file *file)
   if (((void*)(symstr + sym->st_name) >= file->data + file->size)
       || (symstr[sym->st_name] == '\0'))
     return ;
-  printf("%s\n", &symstr[sym->st_name]);
+  printf("%ld %s\n", sym->st_value, &symstr[sym->st_name]);
 }
 
 void	print_elf_info64(Elf64_Ehdr *elf)
